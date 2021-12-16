582d4b18c1dfd8f9511ff40a3d8b867c88a288ee
Neko <57575090+Nekoraru22@users.noreply.github.com>
2021-12-16 21:34:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function one() {
     try {
-        document.getElementsByClassName("textoLink")[1].click()
-        document.getElementsByClassName("dungeonImage")[0].click()
-        document.getElementsByClassName("dungeonImage")[3].click()
+        document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[0].click()
+        document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[3].click()
         document.getElementsByClassName("actionButtons playAndPause")[0].click()
+        document.getElementsByClassName("swal2-confirm swal2-styled")[0].click()
     } catch {
         if (document.getElementsByClassName("swal2-html-container")[0].innerText == "No tienes energía suficiente") {
             document.getElementsByClassName("swal2-confirm swal2-styled")[0].click()
-            document.getElementsByClassName("textoLink")[3].click()
-            try {document.getElementsByClassName("mybtn1 link1 btnAñadirCofres radius5")[1].click()} catch {}
-            console.log("asasasas")
+            document.getElementsByClassName("nav-link notification")[1].click()
+            try {
+                document.getElementsByClassName("mybtn1 link1 btnAñadirCofres radius5")[0].click()
+            } catch {}
             setTimeout(get_rewards, 1000)
         }
     }
@@ -23,15 +24,12 @@ function two() {
 
 function get_rewards() {
     try {
-        console.log("asasasas22")
         for (i=0;i<3;i++) {
-            document.getElementsByClassName("mybtn1 link1 btnAñadirCofres")[i+1].click()
+            document.getElementsByClassName("mybtn1 link1 btnAñadirCofres")[i].click()
         }
-        document.getElementsByClassName("textoLink")[1].click()
-    } catch {document.getElementsByClassName("textoLink")[1].click()}
+    } catch {}
+    document.getElementsByClassName("nav-link")[4].click()
 }
 
 setTimeout(one, 1000)
-setTimeout(two, 10000)
-setTimeout(two, 20000)
-
+setTimeout(two, 10000) // For autoskip the battle
