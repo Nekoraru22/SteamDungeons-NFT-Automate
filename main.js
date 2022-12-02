3b6d362c20720ab40eb381b21994557e470cf055
Neko <57575090+Nekoraru22@users.noreply.github.com>
2022-12-02 23:20:43 +0100

    fixed errors

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 function one() {
-    try {
-        document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[0].click()
-        document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[4].click()
-        document.getElementsByClassName("actionButtons playAndPause")[0].click()
+    document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[0].click()
+    document.getElementsByClassName("col-sm-12 col-md-6 col-lg-4 dungeon")[5].click()
+    
+    // document.getElementsByClassName("actionButtons playAndPause")[0].click()
+    // document.getElementsByClassName("swal2-confirm swal2-styled")[0].click()
+
+    if (document.getElementsByClassName("swal2-html-container")[0].innerText == "No tienes energía suficiente") {
         document.getElementsByClassName("swal2-confirm swal2-styled")[0].click()
-    } catch {
-        if (document.getElementsByClassName("swal2-html-container")[0].innerText == "No tienes energía suficiente") {
-            document.getElementsByClassName("swal2-confirm swal2-styled")[0].click()
-            document.getElementsByClassName("nav-link notification")[2].click()
-            try {
-                document.getElementsByClassName("mybtn1 link1 btnAñadirCofres radius5")[0].click()
-            } catch {}
-            setTimeout(get_rewards, 1000)
-        }
+        document.getElementsByClassName("nav-link notification")[2].click()
+        try {
+            document.getElementsByClassName("mybtn1 link1 btnAñadirCofres radius5")[0].click()
+        } catch {}
+        setTimeout(get_rewards, 1000)
+    }
+    else {
+        document.location.reload()
     }
 }
 
@@ -28,12 +30,12 @@ function get_rewards() {
             document.getElementsByClassName("mybtn1 link1 btnAñadirCofres")[i].click()
         }
     } catch {}
-    document.getElementsByClassName("nav-link")[4].click()
+    document.getElementsByClassName("nav-link")[3].click()
 }
 
 setTimeout(one, 1000)
-setTimeout(two, 10000)
-setTimeout(two, 20000)
-setTimeout(two, 30000)
-setTimeout(two, 40000)
-setTimeout(two, 50000)
+// setTimeout(two, 10000)
+// setTimeout(two, 20000)
+// setTimeout(two, 30000)
+// setTimeout(two, 40000)
+// setTimeout(two, 50000)
